d2b33c5321a9dba41ae7ffefc4215d9537ce7448
Evan Krambuhl <thekrambuhl@gmail.com>
2016-02-07 21:34:43 -0800

    variable object check

--- test/helpers/Empty.js
+++ test/helpers/Empty.js
@@ -2,6 +2,14 @@ var splitTree = require('rogain-tree-utils').splitTree;
 
 module.exports = function(tree, props) {
   var split = splitTree(tree.children, 'component', 'Else');
-  if (tree.data && tree.data.length === 0) return split[0];
+  if (tree.data) {
+    if (Array.isArray(tree.data)) {
+      if (tree.data.length === 0) return split[0];
+    } else if (typeof tree.data === 'object') {
+      if (Object.keys(tree.data).length === 0) return split[0];
+    } else {
+      return split[1];
+    }
+  }
   return split[1];
 };
